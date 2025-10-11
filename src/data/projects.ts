4decc7acb16e6247860a136e69fcb2b30204b2ba
Michie Yagi <kometoume@gmail.com>
2025-10-11 23:30:29 +0900

    ロゴ追加

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,7 +14,7 @@ export interface Project {
 
 // プロジェクトデータの配列
 export const projects: Project[] = [
-      {
+  {
     id: "zunda_web",
     title: "【仙台名物】ずんだ餅専門店Webサイト（自主制作）",
     description:
@@ -23,12 +23,13 @@ export const projects: Project[] = [
     technologies: ["HTML", "JavaScript", "CSS", "Tailwind CSS"],
     demoLink: "https://kometoume.github.io/zunda_site/",
     points: [
+      "Illustratorで、ブランドの信念を象徴するハンコ風ロゴを自作し、伝統と信頼感をデザインで表現。",
       "Google Fontsで和風フォントを選定し、商品名や見出しに伝統的な和菓子の雰囲気と高級感を演出。",
       "モバイルフレンドリーなレスポンシブデザインを、Tailwind CSSのユーティリティを活用して実現。",
-      'Tailwindのデフォルトカラーを上書きし、カスタムの「ずんだカラー」を定義・適用することで、ブランド独自の配色を一貫して表現。',
+      "Tailwindのデフォルトカラーを上書きし、カスタムの「ずんだカラー」を定義・適用することで、ブランド独自の配色を一貫して表現。",
     ],
   },
-    {
+  {
     id: "todo-app",
     title: "毎日掃除リスト（自主制作）",
     description:
@@ -45,11 +46,11 @@ export const projects: Project[] = [
     ],
   },
   {
-    id: "eding-website", 
+    id: "eding-website",
     title: "前職の会社のWebサイト",
     description:
       "前職の会社のWebサイトの制作・改修を担当しました。主にHTML, CSS, JavaScript, jQueryを用いてレスポンシブデザインに対応させ、視覚的な魅力を高めるスライダーを導入しました。バックエンドではPHPを使用してセキュアな問い合わせフォームを実装しています。デザイン面ではAdobe PhotoshopとIllustratorを活用し、サイト全体の品質向上に貢献しました。",
-    image: "/images/eding-thumbnail.png", 
+    image: "/images/eding-thumbnail.png",
     technologies: [
       "HTML",
       "CSS",
@@ -59,7 +60,7 @@ export const projects: Project[] = [
       "Adobe Photoshop",
       "Adobe Illustrator",
     ],
-    demoLink: "https://www.eding.co.jp", 
+    demoLink: "https://www.eding.co.jp",
     points: [
       "レスポンシブデザイン対応",
       "問い合わせフォーム作成",
@@ -67,11 +68,11 @@ export const projects: Project[] = [
     ],
   },
   {
-    id: "client-website-scsk-nvidia", 
+    id: "client-website-scsk-nvidia",
     title: "クライアントのWebサイト (スライダー・シミュレーション)",
     description:
       "クライアントのWebサイトです。クライアントの要望に応えつつ、サイトの顔となるスライダーのデザインや、ユーザー体験を向上させる見積もりシミュレーションの設計・実装を担当しました。",
-    image: "/images/project-a-thumbnail.png", 
+    image: "/images/project-a-thumbnail.png",
     technologies: [
       "HTML",
       "CSS",
@@ -80,7 +81,7 @@ export const projects: Project[] = [
       "Adobe Photoshop",
       "Adobe Illustrator",
     ],
-    demoLink: "https://www.scsk.jp/sp/nvidia/index.html", 
+    demoLink: "https://www.scsk.jp/sp/nvidia/index.html",
     points: [
       "レスポンシブデザイン対応",
       "見積もりシミュレーションの作成",
@@ -90,11 +91,11 @@ export const projects: Project[] = [
     ],
   },
   {
-    id: "client-website-scsk-club", 
+    id: "client-website-scsk-club",
     title: "クライアントのWebサイト (スライダー・デザイン調整)",
     description:
       "クライアントのWebサイトにおいて、トップページの印象を左右するスライダーのデザインから画像制作、実装・導入までを一貫して担当しました。また、既存サイトの構成やデザインの調整にも対応し、ユーザー動線の見直しや導線設計の改善を行いました。",
-    image: "/images/project-b-thumbnail.png", 
+    image: "/images/project-b-thumbnail.png",
     technologies: [
       "HTML",
       "CSS",
@@ -103,15 +104,15 @@ export const projects: Project[] = [
       "Adobe Photoshop",
       "Adobe Illustrator",
     ],
-    demoLink: "https://www.scsk.jp/sp/clubscsk/", 
+    demoLink: "https://www.scsk.jp/sp/clubscsk/",
     points: ["レスポンシブデザイン対応", "スライダー導入", "図版作成"],
   },
   {
-    id: "client-website-scsk-ixia", 
+    id: "client-website-scsk-ixia",
     title: "クライアントのWebサイト (リニューアル)",
     description:
       "クライアントのWebサイトにおいて、サイト全体のデザインを一新するリニューアルプロジェクトに携わり、画像制作、実装・導入までを一貫して担当しました。加えて、サイト全体の構成やデザインの見直しを行い、ユーザー動線を最適化しました。",
-    image: "/images/project-c-thumbnail.png", 
+    image: "/images/project-c-thumbnail.png",
     technologies: [
       "HTML",
       "CSS",
@@ -120,15 +121,15 @@ export const projects: Project[] = [
       "Adobe Photoshop",
       "Adobe Illustrator",
     ],
-    demoLink: "https://www.scsk.jp/sp/ixia/", 
+    demoLink: "https://www.scsk.jp/sp/ixia/",
     points: ["レスポンシブデザイン対応", "図版作成"],
   },
   {
-    id: "client-website-scsk-pureflow", 
+    id: "client-website-scsk-pureflow",
     title: "クライアントのWebサイト (構成・デザイン修正)",
     description:
       "クライアントの要望に応じて、サイト全体の構成やデザインの細部を適宜修正し、図版の作成や既存素材の調整なども行いました。視認性や操作性を意識した改善を重ねることで、ユーザー体験の向上に貢献しています。",
-    image: "/images/project-d-thumbnail.png", 
+    image: "/images/project-d-thumbnail.png",
     technologies: [
       "HTML",
       "CSS",
@@ -137,11 +138,7 @@ export const projects: Project[] = [
       "Adobe Photoshop",
       "Adobe Illustrator",
     ],
-    demoLink: "https://www.scsk.jp/sp/pureflow/index.html", 
+    demoLink: "https://www.scsk.jp/sp/pureflow/index.html",
     points: ["レスポンシブデザイン対応", "図版作成"],
   },
-
-
-
-
 ];
